4140c09ff9daab6cb2f489d0a9e19f3221b183ce
Paul Michaels <pcmichaels@live.com>
2019-10-18 18:11:40 +0100

    Add new project to convert to core

---
+++ WinForms-Core-2/WinForms-Core-2/Utils/CoreFuncs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms_Legacy_1.Utils
+{
+    public class CoreFuncs
+    {
+        // This class hasn't been used for years, but everyone is too frightened to delete it!
+    }
+}
