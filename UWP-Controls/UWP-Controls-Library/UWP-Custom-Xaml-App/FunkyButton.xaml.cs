808fa16c7dae21c476b1aae479579e4a0df8dabf
Paul Michaels <pcmichaels@live.com>
2019-11-17 14:56:55 +0000

    WPF embedded UWP button now working

---
+++ UWP-Controls/UWP-Controls-Library/UWP-Custom-Xaml-App/FunkyButton.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UWP_Custom_Xaml_App
+{
+    public sealed partial class FunkyButton : UserControl
+    {
+        public FunkyButton()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TheButton_Click(object sender, RoutedEventArgs e)
+        {
+            PopAnimation.Begin();
+        }
+    }
+}
